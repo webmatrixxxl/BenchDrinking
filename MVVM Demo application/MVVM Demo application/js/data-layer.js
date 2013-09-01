83a15847fbbb5f97cf824a467d843c2125d20e6e
webmatrixxxl <webmatrix@mail.bg>
2013-09-01 09:06:56 +0300

    One, two, three...

---
+++ MVVM Demo application/MVVM Demo application/js/data-layer.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+
+    var bench = {
+        //Id: "",
+        Name: "",
+        UsersCount: 3,
+        Latitude: 4.0,
+        Longitude: 5.0,
+        StartTime: "",
+        EndTime: ""
+    }
+
+    var benchArray = [
+        {
+            Name: "",
+            UsersCount: 3,
+            Latitude: 4.0,
+            Longitude: 5.0,
+            StartTime: "",
+            EndTime: ""
+        }
+    ]
+
+    function getAllBenches() {
+    }
+
+    WinJS.Namespace.define("Data", {
+        getAllBenches: getAllBenches
+    })
+})();
